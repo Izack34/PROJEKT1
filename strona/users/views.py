9823662d7b25230b0bb05558c9f489d83a32bff7
Izack <s.kacper5@gmial.com>
2020-03-28 14:28:36 +0100

    Changes to be committed: 	modified:   blog/__pycache__/urls.cpython-37.pyc 	modified:   blog/__pycache__/views.cpython-37.pyc 	modified:   blog/templates/blog/base.html 	modified:   blog/templates/blog/home.html 	modified:   blog/templates/blog/post_detail.html 	modified:   blog/templates/blog/tag_posts.html 	modified:   blog/templates/blog/user_posts.html 	modified:   db.sqlite3 	modified:   strona/__pycache__/urls.cpython-37.pyc 	modified:   strona/urls.py 	modified:   users/__pycache__/models.cpython-37.pyc 	modified:   users/__pycache__/views.cpython-37.pyc 	modified:   users/models.py 	modified:   users/templates/users/profile.html 	modified:   users/views.py

--- strona/users/views.py
+++ strona/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
-
+from .models import Profile
 
 def register(request):
     if request.method == 'POST':
@@ -16,8 +16,14 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form': form})
 
+def view_profile(request, pk):
+    context= {
+        'u_profile': Profile.objects.filter(user_id=pk).first()
+    }
+    return render(request, 'users/profile.html', context)
+
 @login_required
-def profile(request):
+def my_profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
@@ -27,7 +33,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request,f'Ypur account has been updated!')
+            messages.success(request,f'Your account has been updated!')
             return redirect('profile')
 
     else:
